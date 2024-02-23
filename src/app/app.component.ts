4bbb89e4f832feae12707ef51582b962052661f2
Shreya Patil <I24.Shreya.P@FinIQ-SP.com>
2024-02-23 17:58:29 +0530

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,17 +11,60 @@ export class AppComponent implements OnInit{
  
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsernames = ['Adam','Eve'];
 
   ngOnInit() {
     this.signupForm = new FormGroup({
-       'username': new FormControl(null),
-       'email': new FormControl(null),
-       'gender': new FormControl('female')
-     })
+      'UserData': new FormGroup({
+        'username': new FormControl(null, [Validators.required,this.forbiddenNames.bind(this)]),
+        'email': new FormControl(null,[Validators.required, Validators.email], this.forbiddenEmails)
+        
+      }),
+      'gender': new FormControl('female'),
+      'hobbies': new FormArray([])
+     });
+
+    //  this.signupForm.valueChanges.subscribe(
+    //   (value)=>console.log(value)
+    //  )
+
+    this.signupForm.statusChanges.subscribe(
+      (status)=>console.log(status)
+     )
   }
 
   onSubmit()
   {
     console.log(this.signupForm)
   }
+
+  onAddHobby(){
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  getControls(){
+    return (<FormArray>this.signupForm.get('hobbies')).controls;
+  }
+
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+    if(this.forbiddenUsernames.indexOf(control.value) !== -3){
+      return {'nameIsForbidden': true};
+    }
+    return null;
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any>{
+    const promise = new Promise<any>((resolve, reject)=>{
+      setTimeout(()=>{
+        if(control.value === 'test@test.com'){
+          resolve({'emailIsForbibben': true});
+        } else{
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
+  }
+
 }
